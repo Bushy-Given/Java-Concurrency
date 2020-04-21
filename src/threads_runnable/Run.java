8e2d24c8fc3834ada4d84131dc04c5b7f29c0150
Bushy <bushy.netshidaulu@gmail.com>
2020-04-21 22:17:46 +0200

    prefered way to shutdown executors

--- src/threads_runnable/Run.java
+++ src/threads_runnable/Run.java
@@ -3,6 +3,7 @@ package threads_runnable;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Bushy-Netshidaulu
@@ -23,6 +24,29 @@ public class Run {
 
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         executorService.submit(task);
+        //shutdown() -> the the excecutorService  after all running tasks are finished
+        executorService.shutdown();
+
+        //shutdownNow() -> interrupts all running tasks and shut the executor down immediately.
+        executorService.shutdownNow();
+
+
+        //This is the preferred way how to typically shutdown executors:
+        try {
+            System.out.println("attempt to shutdown executor");
+            executorService.shutdown();
+            executorService.awaitTermination(5, TimeUnit.SECONDS);
+        }
+        catch (InterruptedException e) {
+            System.err.println("tasks interrupted");
+        }
+        finally {
+            if (!executorService.isTerminated()) {
+                System.err.println("cancel non-finished tasks");
+            }
+            executorService.shutdownNow();
+            System.out.println("shutdown finished");
+        }
 
     }
 }
