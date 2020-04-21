1d7d20109020f17fad37cc16cd620bcf01d28250
Bushy <bushy.netshidaulu@gmail.com>
2020-04-21 23:02:15 +0200

    Any call to future.get() will block and wait until the underlying callable has been terminated. In the worst case a callable runs forever - thus making your application unresponsive. You can simply counteract those scenarios by passing a timeout:

--- src/callable_future/Run.java
+++ src/callable_future/Run.java
@@ -8,7 +8,7 @@ import java.util.concurrent.*;
  */
 public class Run {
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
 
         //This lambda expression defines a callable returning an integer after sleeping for one second:
         Callable<Integer> task = ()-> {
@@ -20,6 +20,8 @@ public class Run {
             }
         };
 
+        //Here we used newFixedThreadPool(1) --This is equivalent to newSingleThreadExecutor()
+        // but we could later increase the pool size by simply passing a value larger than one.
 
         ExecutorService executor = Executors.newFixedThreadPool(1);
 
@@ -28,11 +30,29 @@ public class Run {
 
         // executor.shutdownNow(); *none terminated future will through an exception if shutdownNow is call before get*
 
+
+        //Any call to future.get() will block and wait until the underlying callable has been terminated.
         Integer result = future.get();
 
         System.out.println("future done? " + future.isDone());
         System.out.print("result: " + result);
 
+
+
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+
+        Future<Integer> future1 = executorService.submit(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+                return 456;
+            } catch (InterruptedException ex) {
+                throw new IllegalStateException("Executor Service interrupted!");
+            }
+        });
+
+        //Executing the above code results in a TimeoutException:
+        // We specified a maximum wait time of one second but the callable actually needs two seconds before returning the result.
+        future1.get(1 ,TimeUnit.SECONDS);
 
 
     }
