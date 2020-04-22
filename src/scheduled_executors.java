f08d3c96310b4ee604702f8c85fbb71d514e836f
Bushy <bushy.netshidaulu@gmail.com>
2020-04-22 22:00:20 +0200

    A ScheduledExecutorService is capable of scheduling tasks to run either periodically or once after a certain amount of time has elapsed.

--- src/scheduled_executors.java
+++ src/scheduled_executors.java
@@ -1,10 +1,29 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by Bushy-Netshidaulu
  * on 4/22/2020
  */
 public class scheduled_executors {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        //This code sample schedules a task to run after an initial delay of three seconds has passed:
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+
+        Runnable task = () -> System.out.println("Scheduling: " + System.nanoTime());
+
+        ScheduledFuture<?> future = executorService.schedule(task, 3, TimeUnit.SECONDS);
+
+        TimeUnit.MILLISECONDS.sleep(1337);
+
+        long remainingDelay = future.getDelay(TimeUnit.MILLISECONDS);
+        System.out.printf("Remaining Delay: %sms", remainingDelay);
+
+
 
     }
 }
